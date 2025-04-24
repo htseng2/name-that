3b5128f87d03bccc0dd8d17f57f7a95494d74707
Christopher Tseng <christopher.tseng@frontier.cool>
2025-04-24 09:42:00 +0800

    Enhance index.html with font preconnect links and add custom title styling in App.css. Update App component to display a title with new styles.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <div></div>
+      <div className="title">NAME THAT!</div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
