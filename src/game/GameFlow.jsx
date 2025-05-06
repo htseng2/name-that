ab553e3c4705953c59914f4798995fc2518c03bf
Christopher Tseng <christopher.tseng@frontier.cool>
2025-05-06 17:37:27 +0800

    feat(GameFlow): add Logo component to enhance game flow interface

--- src/game/GameFlow.jsx
+++ src/game/GameFlow.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./GameFlow.css";
+import Logo from "./Logo";
 
 const GameFlow = () => {
   return (
-    <div className="game-flow">{/* TODO: add game flow content here */}</div>
+    <div className="game-flow">
+      <Logo />
+      {/* TODO: add game flow content here */}
+    </div>
   );
 };
 
