65b829d8823e90954383907a21b5657dd70a623a
Christopher Tseng <christopher.tseng@frontier.cool>
2025-05-06 13:28:43 +0800

    feat(App): update edition options to include images and titles
    - refactor EDITION_OPTIONS to be an array of objects containing title and image properties
    - import new edition images for enhanced visual representation
    - update image display logic to show the corresponding image based on selected edition

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,33 @@ import { FaTimes } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import RoundSelector from "./RoundSelector";
+import edition1980s from "./assets/editions/edition-1980s.webp";
+import edition1990s from "./assets/editions/edition-1990s.webp";
+import editionChristmas from "./assets/editions/edition-christmas.webp";
+import editionHalloween from "./assets/editions/edition-halloween.webp";
+import editionTomCruise from "./assets/editions/edition-tom-cruise.webp";
+import editionTVShow from "./assets/editions/edition-tv-show.webp";
+import notAvailable from "./assets/editions/not-available.webp";
 
 const EDITION_OPTIONS = [
-  "1980s Edition",
-  "1990s Edition",
-  "Christmas Edition",
-  "Halloween Edition",
-  "Tom Cruise Edition",
-  "TV Show Edition",
-  "2000s Edition",
-  "1970s Edition",
-  "Movie Star Edition",
-  "Music Icons Edition",
-  "Sports Legends Edition",
-  "Sci-Fi Edition",
-  "Disney Edition",
-  "Sitcom Edition",
-  "Oscar Winners Edition",
-  "Superhero Edition",
-  "Action Movies Edition",
-  "Anime Edition",
+  { title: "1980s Edition", img: edition1980s },
+  { title: "1990s Edition", img: edition1990s },
+  { title: "Christmas Edition", img: editionChristmas },
+  { title: "Halloween Edition", img: editionHalloween },
+  { title: "Tom Cruise Edition", img: editionTomCruise },
+  { title: "TV Show Edition", img: editionTVShow },
+  { title: "2000s Edition" },
+  { title: "1970s Edition" },
+  { title: "Movie Star Edition" },
+  { title: "Music Icons Edition" },
+  { title: "Sports Legends Edition" },
+  { title: "Sci-Fi Edition" },
+  { title: "Disney Edition" },
+  { title: "Sitcom Edition" },
+  { title: "Oscar Winners Edition" },
+  { title: "Superhero Edition" },
+  { title: "Action Movies Edition" },
+  { title: "Anime Edition" },
 ];
 
 function App() {
@@ -67,7 +74,7 @@ function App() {
                     }`}
                     onClick={() => setSelectedIndex(globalIdx)}
                   >
-                    {option}
+                    {option.title}
                   </div>
                 );
               }
@@ -98,7 +105,10 @@ function App() {
           </button>
         </div>
         <div className="image-display">
-          <img src="https://placehold.co/370x370" alt="placeholder" />
+          <img
+            src={EDITION_OPTIONS[selectedIndex]?.img ?? notAvailable}
+            alt={EDITION_OPTIONS[selectedIndex]?.title || "placeholder"}
+          />
         </div>
         <button
           type="button"
