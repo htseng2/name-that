7c8404757ec48022fe8c70486301cfd88b8b302c
Christopher Tseng <christopher.tseng@frontier.cool>
2025-05-07 16:13:34 +0800

    refactor(App): move edition options to constants for better organization
    - remove inline edition options from App.jsx
    - import EDITION_OPTIONS from constants for cleaner code structure
    - streamline edition selection logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,8 @@ import { FaGear } from "react-icons/fa6";
 import { FaTimes } from "react-icons/fa";
 import RoundSelector from "./components/RoundSelector";
 import Logo from "./components/Logo";
-import edition1980s from "./assets/editions/edition-1980s.webp";
-import edition1990s from "./assets/editions/edition-1990s.webp";
-import editionChristmas from "./assets/editions/edition-christmas.webp";
-import editionHalloween from "./assets/editions/edition-halloween.webp";
-import editionTomCruise from "./assets/editions/edition-tom-cruise.webp";
-import editionTVShow from "./assets/editions/edition-tv-show.webp";
+import { EDITION_OPTIONS } from "./constants";
 import notAvailable from "./assets/editions/not-available.webp";
-
-const EDITION_OPTIONS = [
-  { title: "1980s Edition", img: edition1980s },
-  { title: "1990s Edition", img: edition1990s },
-  { title: "Christmas Edition", img: editionChristmas },
-  { title: "Halloween Edition", img: editionHalloween },
-  { title: "Tom Cruise Edition", img: editionTomCruise },
-  { title: "TV Show Edition", img: editionTVShow },
-  { title: "2000s Edition" },
-  { title: "1970s Edition" },
-  { title: "Movie Star Edition" },
-  { title: "Music Icons Edition" },
-  { title: "Sports Legends Edition" },
-  { title: "Sci-Fi Edition" },
-  { title: "Disney Edition" },
-  { title: "Sitcom Edition" },
-  { title: "Oscar Winners Edition" },
-  { title: "Superhero Edition" },
-  { title: "Action Movies Edition" },
-  { title: "Anime Edition" },
-];
 
 function App() {
   const pageSize = 6;
---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+import edition1980s from "../assets/editions/edition-1980s.webp";
+import edition1990s from "../assets/editions/edition-1990s.webp";
+import editionChristmas from "../assets/editions/edition-christmas.webp";
+import editionHalloween from "../assets/editions/edition-halloween.webp";
+import editionTomCruise from "../assets/editions/edition-tom-cruise.webp";
+import editionTVShow from "../assets/editions/edition-tv-show.webp";
+
+export const EDITION_OPTIONS = [
+  { title: "1980s Edition", img: edition1980s },
+  { title: "1990s Edition", img: edition1990s },
+  { title: "Christmas Edition", img: editionChristmas },
+  { title: "Halloween Edition", img: editionHalloween },
+  { title: "Tom Cruise Edition", img: editionTomCruise },
+  { title: "TV Show Edition", img: editionTVShow },
+  { title: "2000s Edition" },
+  { title: "1970s Edition" },
+  { title: "Movie Star Edition" },
+  { title: "Music Icons Edition" },
+  { title: "Sports Legends Edition" },
+  { title: "Sci-Fi Edition" },
+  { title: "Disney Edition" },
+  { title: "Sitcom Edition" },
+  { title: "Oscar Winners Edition" },
+  { title: "Superhero Edition" },
+  { title: "Action Movies Edition" },
+  { title: "Anime Edition" },
+];
