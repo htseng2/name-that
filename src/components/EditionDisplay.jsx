84e336d170c0401b437e1d331869d384d200fcac
Christopher Tseng <htseng2@gmail.com>
2025-05-16 11:24:57 +0800

    style(EditionDisplay): update dimensions for responsive layout
    - adjust width and height properties for the EditionDisplay component to enhance responsiveness across various screen sizes
    - implement Tailwind CSS classes for improved layout consistency and maintainability

--- src/components/EditionDisplay.jsx
+++ src/components/EditionDisplay.jsx
@@ -6,7 +6,7 @@ function EditionDisplay({ edition }) {
   const imgSrc = edition?.img ?? notAvailable;
   const altText = edition?.title ?? "placeholder";
   return (
-    <div className="col-start-4 col-span-3 row-start-2 w-[370px] h-[370px] justify-self-center self-center border-4 border-[#053b60]">
+    <div className="col-start-4 col-span-3 row-start-2 w-[198px] h-[198px] w1194:w-[386px] w1194:h-[386px] w1280:w-[492px] w1280:h-[492px] w1366:w-[370px] w1366:h-[370px] w1440:w-[492px] w1440:h-[492px] w1920:w-[588px] w1920:h-[588px] w2560:w-[750px] w2560:h-[750px] justify-self-center self-center border-4 border-[#053b60]">
       <img
         src={imgSrc}
         alt={altText}
